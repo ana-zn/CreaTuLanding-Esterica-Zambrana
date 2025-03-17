dbf67fd15bcbf552a4f0953512b1bb27d9db1e4c
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-03-17 19:41:28 -0300

    Se modificaron los productos

--- src/productos.js
+++ src/productos.js
@@ -1,42 +1,82 @@
 export const productos = [
     {
         id: 1, 
-        nombre: "Producto 1", 
-        precio: 100, 
-        img: "public/images/1.jpg", 
+        nombre: "Base Líquida Matte", 
+        precio: 5500, 
+        img: "public/images/base1.jpg", 
         stock: 10,
         oferta: true,
-        descripcion: "Descripción del producto 1",
-        categoria: "Labiales"
+        descripcion: "Base líquida de cobertura media con gotero para una aplicación precisa. Ideal para unificar el tono de piel y disimular imperfecciones.",
+        categoria: "rostro"
     }, 
     {
         id: 2, 
-        nombre: "Producto 2", 
-        precio: 100, 
-        img: "public/images/2.jpg",
-        stock: 10,
-        oferta: true,
-        descripcion: "Descripción del producto 1",  
-        categoria: "Rubores"
+        nombre: "Paleta de Sombras Nude", 
+        precio: 4500, 
+        img: "public/images/sombras1.jpg",
+        stock: 5,
+        oferta: false, 
+        descripcion: " Paleta de sombras con tonos neutros y cálidos. Texturas mate y satinadas para crear looks naturales y sofisticados.",  
+        categoria: "Ojos"
     }, 
     {
         id: 3, 
-        nombre: "Producto 3", 
-        precio: 100, 
-        img: "public/images/3.jpg",
-        stock: 10,
+        nombre: "Máscara y Delineadores Black", 
+        precio: 6000, 
+        img: "public/images/Kit1.jpg",
+        stock: 15,
         oferta: true,
-        descripcion: "Descripción del producto 1",
-        categoria:"Bases"
+        descripcion: "Máscara para pestañas y delineador para definir y destacar la mirada. Fórmula de larga duración y resistente al agua.",
+        categoria:"ojos"
     },
     {
         id: 4, 
-        nombre: "Producto 4", 
-        precio: 100, 
-        img: "public/images/4.jpg",
+        nombre: "Labiales Mate en Barra", 
+        precio: 1500, 
+        img: "public/images/pintalabios1.jpeg",
         stock: 10,
         oferta: true,
-        descripcion: "Descripción del producto 1",  
+        descripcion: "Labiales mate en tonos vibrantes. Textura suave y fórmula de larga duración para un color intenso y uniforme.",  
+        categoria: "labios"
+    },
+    {
+        id: 5, 
+        nombre: "Rubores Brown", 
+        precio: 2500, 
+        img: "public/images/rubor1.jpg", 
+        stock: 10,
+        oferta: true,
+        descripcion: "Rubores tonos tierra dan un brillo unico. Apto para todo tipo de piel.",
+        categoria: "Rostro"
+    }, 
+    {
+        id: 6, 
+        nombre: "Gloss Cherry", 
+        precio: 1800, 
+        img: "public/images/pintalabios2.jpeg",
+        stock: 10,
+        oferta: true,
+        descripcion: "Brillo labial líquido con acabado gloss y pigmentación intensa. Hidrata los labios y los deja con un aspecto jugoso.",  
+        categoria: "labios"
+    }, 
+    {
+        id: 7, 
+        nombre: "Producto 3Rubores Compactos", 
+        precio: 1800, 
+        img: "public/images/rubor2.jpg",
+        stock: 10,
+        oferta: true,
+        descripcion: " Rubores compactos de acabado mate para dar color y frescura al rostro. Disponible en tonos cálidos y rosados.",
+        categoria:"rostro"
+    },
+    {
+        id: 8, 
+        nombre: "Paleta de Sombras Doradas", 
+        precio: 3800, 
+        img: "public/images/sombras2.jpg",
+        stock: 10,
+        oferta: true,
+        descripcion: "Paleta de sombras con tonos dorados y cobrizos. Texturas metálicas y satinadas para crear looks glamorosos.",  
         categoria: "4"
     }
 ];
